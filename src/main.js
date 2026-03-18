82793f18d7b38cceba82b8edca4ac675ed2f2a1f
bharath2 <sbarat796@gmail.com>
2026-03-18 06:56:37 +0530

    changes done

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import './style.css'
 import javascriptLogo from './assets/javascript.svg'
 import viteLogo from './assets/vite.svg'
+import palmLogo from './assets/logo.png'
 import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
 <section id="center">
   <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src=${viteLogo} class="vite" alt="Vite logo" />
+    <img src="${palmLogo}" class="palm-logo" alt="The Palm Logo">
   </div>
   <div>
     <h1>Get started</h1>
